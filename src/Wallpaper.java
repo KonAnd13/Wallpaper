8755b5f6ebb1c66d1da5c98c7d11d8d7821b51bb
Andrew <55397891+KonAnd13@users.noreply.github.com>
2019-09-20 01:57:11 +0300

    Update Wallpaper.java

--- src/Wallpaper.java
+++ src/Wallpaper.java
@@ -8,13 +8,10 @@ public class Wallpaper {
         double rollWidth = 0.85;
         int rollLength = 10;
 
-//        How many total panels do you need for gluing a room?
         int numberOfPanels = (int) (roomPerimeter/rollWidth + 1);
 
-//        How many sheets will one roll have?
         int panelsFromOneRoll = (int) (rollLength/roomHeight);
 
-//        How many rolls of wallpaper will you need?
         int numberOfRolls = numberOfPanels/panelsFromOneRoll + 1;
         System.out.println("We need " + numberOfRolls + " rolls of wallpaper.");
     }
